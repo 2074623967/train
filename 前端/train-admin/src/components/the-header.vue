c9c5ebf9e50dc8c11ce9d614afeedc8e62715cf5
tangchungjiao <2074623967@qq.com>
2024-01-15 17:20:41 +0800

    admin去掉登录界面、乘车人界面，去掉登录拦截

--- 前端/train-admin/src/components/the-header.vue
+++ 前端/train-admin/src/components/the-header.vue
@@ -2,13 +2,10 @@
   <a-layout-header class="header">
     <div class="logo">
       <router-link to="/welcome" style="color: white; font-size: 18px">
-        甲蛙12306
+        甲蛙12306控台
       </router-link>
     </div>
-    <div style="float: right; color: white">
-      您好：{{ member.mobile }} &nbsp;&nbsp;
-      <router-link to="/login" style="color: white"> 退出登录 </router-link>
-    </div>
+    <div style="float: right; color: white">欢迎使用管理控台</div>
     <a-menu
       v-model:selectedKeys="selectedKeys"
       theme="dark"
@@ -20,30 +17,8 @@
           <coffee-outlined /> &nbsp; 欢迎
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined /> &nbsp; 乘车人管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/ticket">
-        <router-link to="/ticket">
-          <border-outer-outlined /> &nbsp; 余票查询
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/my-ticket">
-        <router-link to="/my-ticket">
-          <idcard-outlined /> &nbsp; 我的车票
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/seat">
-        <router-link to="/seat">
-          <usergroup-add-outlined /> &nbsp; 座位销售图
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin">
-        <router-link to="/admin">
-          <desktop-outlined /> &nbsp; 关于控台管理
-        </router-link>
+      <a-menu-item key="/about">
+        <router-link to="/about"> <user-outlined /> &nbsp; 关于 </router-link>
       </a-menu-item>
     </a-menu>
   </a-layout-header>
@@ -51,13 +26,11 @@
 
 <script>
 import { defineComponent, ref, watch } from 'vue';
-import store from '@/store';
 import router from '@/router';
 
 export default defineComponent({
   name: 'the-header-view',
   setup() {
-    const member = store.state.member;
     const selectedKeys = ref([]);
 
     watch(
@@ -70,7 +43,6 @@ export default defineComponent({
       { immediate: true }
     );
     return {
-      member,
       selectedKeys,
     };
   },
