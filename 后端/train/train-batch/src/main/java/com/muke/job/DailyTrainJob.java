3977deb69b5771876e90a2b608ac61e57162926c
tangchungjiao <2074623967@qq.com>
2024-01-18 16:24:36 +0800

    增加生成每日车次定时任务

---
+++ 后端/train/train-batch/src/main/java/com/muke/job/DailyTrainJob.java
@@ -0,0 +1,29 @@
+package com.muke.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+/**
+ * 每日车次定时任务
+ *
+ * @author tangcj
+ * @date 2024/01/18 16:17
+ **/
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成车次数据开始");
+    }
+}
