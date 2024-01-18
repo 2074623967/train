2317f49e5ab12f193359f46c3e1664d313b25d06
tangchungjiao <2074623967@qq.com>
2024-01-18 10:35:28 +0800

    项目增加batch模块并且调整代码生成器代码

---
+++ 后端/train/train-batch/src/main/java/com/muke/BatchApplication.java
@@ -0,0 +1,30 @@
+package com.muke;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * 定时调度模块
+ *
+ * @author tangcj
+ * @date 2024/01/18 10:20
+ **/
+@SpringBootApplication(scanBasePackages = {"com.muke"})
+@MapperScan(basePackages = {"com.muke.mapper"})
+public class BatchApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("地址: \thttp://127.0.0.1:{}{}", env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path"));
+    }
+}
