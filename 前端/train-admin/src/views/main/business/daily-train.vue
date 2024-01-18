b28715c16e28a8a068741ef05aa4b50e5cfa9d20
tangchungjiao <2074623967@qq.com>
2024-01-18 15:19:36 +0800

    为每日车次添加车站下拉选择，并自动填充车次相关数据，新增日期、车次查询条件

--- 前端/train-admin/src/views/main/business/daily-train.vue
+++ 前端/train-admin/src/views/main/business/daily-train.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="operateButton">
     <a-space>
+      <a-date-picker
+        v-model:value="params.date"
+        valueFormat="YYYY-MM-DD"
+        placeholder="请选择日期"
+      />
+      <train-select-view
+        v-model="params.code"
+        width="200px"
+      ></train-select-view>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
@@ -55,7 +64,10 @@
         />
       </a-form-item>
       <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrain.code" />
+        <train-select-view
+          v-model="dailyTrain.code"
+          @change="onChangeCode"
+        ></train-select-view>
       </a-form-item>
       <a-form-item label="车次类型">
         <a-select v-model:value="dailyTrain.type">
@@ -102,9 +114,11 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import { notification } from 'ant-design-vue';
 import axios from 'axios';
+import TrainSelectView from '@/components/train-select';
 
 export default defineComponent({
   name: 'daily-train-view',
+  components: { TrainSelectView },
   setup() {
     const TRAIN_TYPE_ARRAY = window.TRAIN_TYPE_ARRAY;
     const visible = ref(false);
@@ -130,6 +144,9 @@ export default defineComponent({
       pageSize: 10,
     });
     const loading = ref(false);
+    const params = ref({
+      code: null,
+    });
     const columns = [
       {
         title: '日期',
@@ -240,6 +257,8 @@ export default defineComponent({
           params: {
             page: param.page,
             size: param.size,
+            code: params.value.code,
+            date: params.value.date,
           },
         })
         .then(response => {
@@ -265,6 +284,14 @@ export default defineComponent({
       });
     };
 
+    const onChangeCode = train => {
+      console.log('车次下拉组件选择：', train);
+      const t = window.Tool.copy(train);
+      delete t.id;
+      // 用assign可以合并
+      dailyTrain.value = Object.assign(dailyTrain.value, t);
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -286,6 +313,8 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
+      onChangeCode,
+      params,
     };
   },
 });
