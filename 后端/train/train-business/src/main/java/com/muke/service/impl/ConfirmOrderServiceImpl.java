aab048d6164a45c6f6fa7277d52e0bbe24f42d33
tangchungjiao <2074623967@qq.com>
2024-01-25 15:07:54 +0800

    sentinel限流使用

--- 后端/train/train-business/src/main/java/com/muke/service/impl/ConfirmOrderServiceImpl.java
+++ 后端/train/train-business/src/main/java/com/muke/service/impl/ConfirmOrderServiceImpl.java
@@ -8,6 +8,8 @@ import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -103,6 +105,8 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
         confirmOrderMapper.deleteByPrimaryKey(id);
     }
 
+    //@SentinelResource("doConfirm")
+    @SentinelResource(value = "doConfirm",blockHandler = "doConfirmBlock")
     @Override
     public void doConfirm(ConfirmOrderDoReq req) {
         String lockKey = DateUtil.formatDate(req.getDate()) + "-" + req.getTrainCode();
@@ -243,11 +247,12 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
             } catch (Exception e) {
                 throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_EXCEPTION);
             }
-            LOG.info("购票流程结束，释放锁！lockKey:{}", lockKey);
-            stringRedisTemplate.delete(lockKey);
+
 //        } catch (Exception e) {
 //            LOG.error("购票异常", e);
         } finally {
+            LOG.info("购票流程结束，释放锁！lockKey:{}", lockKey);
+            stringRedisTemplate.delete(lockKey);
 //            LOG.info("购票流程结束，释放锁！");
 //            if (null != lock && lock.isHeldByCurrentThread()) {
 //                lock.unlock();
@@ -452,5 +457,15 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
                     break;
             }
         }
+    }
+
+    /**
+     * 降级方法，需包含限流方法的所有参数和BlockException参数
+     * @param req
+     * @param e
+     */
+    public void doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        LOG.info("购票请求被限流：{}", req);
+        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
     }
 }
